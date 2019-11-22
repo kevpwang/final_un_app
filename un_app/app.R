0e194c1de051df44fef066c541ce2402047154cb
Kevin Wang <kpwang@college.harvard.edu>
2019-11-21 23:17:06 -0500

    Added statistical choices & commented.

--- un_app/app.R
+++ un_app/app.R
@@ -1,22 +1,19 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(scales)
 library(ggthemes)
 library(tidyverse)
 
+# load relevant RDS for use in plot
+
 majs_gdps <- read_rds("majs_gdps.rds")
+
+# list of country choices for the drop-down menu
 
 country_choices <- majs_gdps %>% 
     distinct(country_name) %>% 
     arrange()
+
+####### UI #######
 
 ui <- fluidPage(
     navbarPage(
@@ -57,6 +54,10 @@ ui <- fluidPage(
             title = "Model",
             sidebarPanel(
                 p("Select a country to see the relationship between its economy and UNGA voting power."),
+                
+                # used later in plot section to receive country choice
+                # set default value with selected, same with value in checkboxInput()
+                
                 selectInput(
                     inputId = "country",
                     label = "Country:",
@@ -74,6 +75,9 @@ ui <- fluidPage(
                 )
             ),
             mainPanel(plotOutput("UNPlot")),
+
+            # fix this later: line breaks seem to have no effect here
+
             br(),
             br(),
             br(),
@@ -82,26 +86,54 @@ ui <- fluidPage(
             verbatimTextOutput("summary")
         ),
         tabPanel(
-            title = "Statistical Choices"
+            title = "Statistical Choices",
+            h3("Initial explorations"),
+            p(
+                "I began the project by trying to explore and visualize how often different countries voted as part of a majority in the UN General Assembly. Even given the symbolic value of many UNGA resolutions, one can reasonably expect that how often a country 'gets its way'--i.e. sees the desired success or failure of a particular agenda item--relates to its international standing and how it is viewed by the international community. One striking trend I immediately found was how infrequently the US is part of a UNGA majority. In a few years, the US votes with the majority on up to 75 percent of votes. Far more frequently, however, the US votes with the majority in under 20 percent of votes--in other words, the US disagrees with the international consensus up to 80 percent of the time."
+            ),
+            h3("Data-processing choices"),
+            p(
+                "I wondered whether a country's economic growth would be associated with more UNGA majority votes, which I am assuming as a reasonable proxy for some dimension of international influence. I used data from the World Bank containing percent change in GDP for all countries since 1961. The data contained many blanks; this is to be expected, since many countries likely did not or were not able to collect accurate annual economic data for the entirety of the past five decades. Therefore, I decided to limit the countries in the model to only those for which there exists annual data from 1961 to 2018."
+            ),
+            p(
+                "Analyzing Erik Voeten's raw data of all UNGA roll call votes, I created a clean dataset of the proportion of all votes for which a country was in the majority ('majorities data'). Because the GDP data had to be restricted to countries that had continuous data going back to 1961, I also restricted my majorities data to 1961 at the earliest. I also considered that 'yes' and 'no' are not the only possible votes. A country can either vote 'abstain' or be absent, though obviously neither ever formed a majority vote. I decided to exclude a country's absentions or absences from the total number of votes in which they participated. This was largely because in certain early UNGA sessions, some countries like Albania only cast 'abstain' or were absent. Therefore, to include those votes would result in an anomalously low majority rate that is attributable to idiosyncratic circumstances, not to the explanatory variable of economic strength."
+            ),
+            h3("Regression"),
+            p(
+                "For the purposes of this rough draft, my model uses a linear regression for each country, with GDP change as the explanatory variable and proportion of votes in the majority as the response variable. The user also has an option to view the summary of the model for more detailed statistical information. The results of the model vary significantly across different countries. For the US and China, the model suggests that higher economic growth is associated with a greater proportion of majority votes. Other countries like Australia and the UK show a slight negative association."
+            ),
+            p(
+                "For a country like China, the association makes intuitive sense: China's economy has grown in tandem with other factors like military strength and diplomatic strength that could affect its international standing generally. For other countries, a negative association could suggest economic decline accompanied by loss of importance, but specific investigation is needed to determine exactly how circumstances change and whether they justify the association. For small or internationally inconsiderable countries, the association is more likely to be random chance rather than any sort of deep pattern."
+            )
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$UNPlot <- renderPlot({
         plot <- majs_gdps %>% 
+            
+        # filter country_name based on received input from user
+            
             filter(str_detect(country_name, input$country)) %>% 
             ggplot(aes(x = growth, y = prop_maj)) +
             geom_point() +
             geom_smooth(method = "lm", se = FALSE, size = 1) +
             scale_x_continuous(labels = percent) +
             scale_y_continuous(labels = percent) +
+            
+            # NB: concatenate strings with paste(), default sep = " "
+            # use user input to indicate that the plot is the right country
+            
             labs(
                 title = "Economic Growth vs. Frequency in UNGA Majority",
+                subtitle = paste(input$country, ", 1961-2018", sep = ""),
                 caption = 'Source: Erik Voeten, "Data and Analyses of Voting in the UN General Assembly"'
             ) + 
+            
+            # NB: fivethirtyeight() automatically hides x- and y-axis labels
+            
             theme_fivethirtyeight() +
             
             # NB: use margin to shift elements up, down, &c. Top margin is first value.  
@@ -109,11 +141,17 @@ server <- function(input, output) {
             theme(
                 plot.caption = element_text(margin = margin(20,1,1,1))
             )
+        
+        # make sure to display separately
+        
         plot
     })
     
     output$summary <- renderPrint({
         if (input$summary == TRUE) {
+            
+            # NB: cannot pipe into lm()
+            
             country <- majs_gdps %>% 
                 filter(str_detect(country_name, input$country))
             model <- lm(prop_maj ~ growth, data = country)
