0fba25b497d3e6f009459a269c05255a79dc3415
Kevin Wang <kpwang@college.harvard.edu>
2019-11-21 21:18:01 -0500

    Changed countryname to country codes.

--- prep_all_majs.R
+++ prep_all_majs.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 un_cleaned <- read_rds("clean-data/un_cleaned.rds")
 
-votes <- un_data %>%
+votes <- un_cleaned %>%
   filter(vote %in% c(1, 3)) %>%
   mutate(vote = recode(vote, `1` = "yes", `3` = "no")) %>%
   select(year, country, countryname, vote, yes, no, unres, short)
@@ -14,14 +14,14 @@ votes <- un_data %>%
 yes_no <- votes %>% select(yes, no)
 
 total_votes <- votes %>%
-  group_by(countryname, year) %>% 
+  group_by(country, year) %>% 
   summarize(total = n())
 
 all_majs <- votes %>% 
   mutate(in_minority = ifelse(colnames(yes_no)[max.col(yes_no)] != vote, TRUE, FALSE)) %>%
-  group_by(countryname, year) %>%
+  group_by(country, year) %>%
   summarize(maj = sum(in_minority == FALSE)) %>% 
-  inner_join(total_votes, by = c("countryname", "year")) %>%
+  inner_join(total_votes, by = c("country", "year")) %>%
   mutate(prop_maj = maj / total)
 
 write_rds(all_majs, "clean-data/all_majs.rds")
