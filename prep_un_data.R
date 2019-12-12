25ed556acf3879175cc97c2f274ecae54c3ad9ec
Kevin Wang <kpwang@college.harvard.edu>
2019-12-11 20:31:56 -0500

    Added character encoding

--- prep_un_data.R
+++ prep_un_data.R
@@ -1,9 +1,11 @@
+library(tidyverse)
+library(janitor)
 
 # some votes have the `no` column as NA. Since `yes` votes
 # on those are overwhelmingly, reasonable to assume that
 # the NAs is a data error and replace NAs with 0
 
-un_data <- read.csv("raw-data/un_data.csv") %>%
+un_data <- read.csv("raw-data/un_data.csv", fileEncoding = "latin1") %>%
   clean_names() %>%
   arrange(year) %>%
   replace_na(list(no = 0))
